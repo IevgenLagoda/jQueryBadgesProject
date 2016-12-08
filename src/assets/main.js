25b2fe4e1a8f60425140dfdbca3f2fae5c2837b1
Eugene Lagoda <eugene.lagoda@gmail.com>
2016-12-08 15:37:56 +0200

    done

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,20 @@
 $(function() {
 
-  // your code will go here
+
+    $.ajax({
+        url: 'https://www.codeschool.com/users/nmagog.json',
+        dataType: 'jsonp',
+        success: function(response) {
+            response.courses.completed.map(function(item) {
+                $('#badges').append(
+                    `<div class="course">
+                    <h3>${item.title}</h3>
+                    <img src="${item.badge }"/>
+                    <a href="${item.url}" target="_blank" class="btn btn-primary">See course</a>
+                  </div>`
+                );
+            });
+        }
+    });
 
 });
